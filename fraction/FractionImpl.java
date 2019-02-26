d5c8593b84435d07f3f88999d4f91baec5a13431
dlewis11bbk <32481406+dlewis11bbk@users.noreply.github.com>
2019-02-26 21:02:34 +0000

    Comments added. Now immutable, helper class tuple

--- fraction/FractionImpl.java
+++ fraction/FractionImpl.java
@@ -1,35 +1,37 @@
 package fraction;
-import java.lang.Math.*;
-
+/**
+ * Implementation of Fraction interface for POP2 coursework
+ * Representing fractions of the form numerator and denominator
+ * The instance objects are immutable.
+ */
 public final class FractionImpl implements Fraction {
-    /**
-     * Parameters are the <em>numerator</em> and the <em>denominator</em>.
-     * Normalize the fraction as you create it.
-     * For instance, if the parameters are <pre>(8, -12)</pre>, create a <pre>fraction</pre> with numerator
-     * <pre>-2</pre> and denominator <pre>3</pre>.
-     *
-     * The constructor should throw an <pre>ArithmeticException</pre> if the denominator is zero.
-     *
-     * @param numerator
-     * @param denominator
-     */
-
-    // Fractions should be immutable
+    // Instance variables can only be set once.
     private final int numerator;
     private final int denominator;
-    private final int NUM_POS = 0;
-    private final int DENOM_POS = 1;
 
-
+    /**
+     * Parameters are the <em>numerator</em> and the <em>denominator</em> of a fraction.
+     * Fraction are created in a normalised form.
+     * If the parameters are <pre>(8, -12)</pre>, a <pre>fraction</pre> with numerator
+     * <pre>-2</pre> and denominator <pre>3</pre> is created. i.e. negative fractions can be created
+     * by using a negative numerator or denominator.
+     * Using both a negative numerator and denominator results in a positive fraction.
+     * The constructor throws an <pre>ArithmeticException</pre> if the denominator is zero.
+     *
+     * @param numerator the numerator of the fraction
+     * @param denominator the denominator of the fraction
+     */
     public FractionImpl(int numerator, int denominator) {
-        int fracArray[] = this.normalise(numerator, denominator);
-        this.numerator = fracArray[NUM_POS];
-        this.denominator = fracArray[DENOM_POS];
+        this.checkZeroD(denominator);
+        Tuple f = new Tuple(numerator, denominator);
+        this.manage_negs(f);
+        this.normalise(f);
+        this.numerator = f.n;
+        this.denominator = f.d;
     }
 
     /**
      * The parameter is the numerator and <pre>1</pre> is the implicit denominator.
-     *
      * @param wholeNumber representing the numerator
      */
     public FractionImpl(int wholeNumber) {
@@ -40,29 +42,50 @@ public final class FractionImpl implements Fraction {
     /**
      * The parameter is a <pre>String</pre> containing either a whole number, such as `5` or `-3`, or a fraction,
      * such as "8/-12".
-     * Allow blanks around (but not within) integers.
-     * The constructor should throw an <pre>ArithmeticException</pre>
+     * Space characters are allowed around (but not within) integers.
+     * The constructor throw an <pre>ArithmeticException</pre>
      * if given a string representing a fraction whose denominator is zero.
-     * <p>
-     * You may find it helpful to look at the available String API methods in the Java API.
-     *
+     * negative fractions can be created
+     * by using a negative numerator or denominator.
+     * Using both a negative numerator and denominator results in a positive fraction.
      * @param fraction the string representation of the fraction
      */
     public FractionImpl(String fraction) {
-        String fracSplit[] = fraction.trim().split("/");
-        int n, d;
+        final int NUM_POS = 0;
+        final int DENOM_POS = 1;
+        String[] fracSplit = fraction.trim().split("/");
+        // Only numerator is given in string
+        // In this case can be a zero
         if (fracSplit.length == 1){
             this.numerator = Integer.parseInt(fracSplit[NUM_POS]);
             this.denominator = 1;
         }
+        // Numerator and denominator given in string
         else {
-            n = Integer.parseInt(fracSplit[NUM_POS]);
+            int n, d;
             d = Integer.parseInt(fracSplit[DENOM_POS]);
-            int fracArray[] = this.normalise(n, d);
-            this.numerator = fracArray[NUM_POS];
-            this.denominator = fracArray[DENOM_POS];
+            this.checkZeroD(d);
+            n = Integer.parseInt(fracSplit[NUM_POS]);
+            Tuple f = new Tuple(n, d);
+            this.manage_negs(f);
+            this.normalise(f);
+            this.numerator = f.n;
+            this.denominator = f.d;
         }
+    }
 
+    /**
+     * A helper class to make passing
+     * two int vars convenient
+     */
+    private static class Tuple {
+        private int n;
+        private int d;
+
+        private Tuple(int a, int b){
+            this.n = a;
+            this.d = b;
+        }
     }
 
     /**
@@ -122,10 +145,7 @@ public final class FractionImpl implements Fraction {
      */
     @Override
     public Fraction abs() {
-        int a, b;
-        a = this.numerator;
-        b = this.denominator;
-        return new FractionImpl(Math.abs(a), Math.abs(b));
+        return new FractionImpl(Math.abs(this.numerator), Math.abs(this.denominator));
     }
 
     /**
@@ -133,10 +153,7 @@ public final class FractionImpl implements Fraction {
      */
     @Override
     public Fraction negate() {
-        int n;
-        n = this.numerator;
-        n = -n;
-        return new FractionImpl(n, this.denominator);
+        return new FractionImpl(-this.numerator, this.denominator);
     }
 
     /**
@@ -151,7 +168,6 @@ public final class FractionImpl implements Fraction {
      * @inheritDoc
      */
     @Override
-
     public boolean equals(Object obj) {
         if (obj instanceof Fraction){
             FractionImpl f = (FractionImpl)obj;
@@ -183,13 +199,14 @@ public final class FractionImpl implements Fraction {
     /**
      * @inheritDoc
      */
-    @Override
-    /*
-    This method returns:
-    A negative int if this is less than o.
-    Zero if this is equal to o.
-    A positive int if this is greater than o
+
+    /**
+     * This method returns:
+     * A negative int if this is less than o.
+     * Zero if this is equal to o.
+     * A positive int if this is greater than o
      */
+    @Override
     public int compareTo(Fraction o) {
         FractionImpl temp = (FractionImpl) o;
         int a, b, c, d;
@@ -200,7 +217,6 @@ public final class FractionImpl implements Fraction {
         return ((a * d) - (c * b));
     }
 
-
     /**
      * @inheritDoc
      */
@@ -209,29 +225,52 @@ public final class FractionImpl implements Fraction {
         return this.numerator + "/" + this.denominator;
     }
 
+
     /**
-     * @inheritDoc
+     * Manages constructor with denominator given as zero
+     *
+     * @param d an integer representing a denominator
+     * @exception ArithmeticException
      */
-    private int[] normalise(int n, int d) {
-        int i;
-        int normFract[] = new int[2];
+    private void checkZeroD(int d){
         if (d == 0){
             throw new ArithmeticException("Divide by zero!");
         }
-
-        i = this.GCD(Math.abs(n), Math.abs(d));
-
-        if (d < 0) {
-            n *= -1;
-            d *= -1;
-        }
-        normFract[0] = n / i;
-        normFract[1] = d / i;
-        return normFract;
     }
 
     /**
-     * @inheritDoc
+     * Normalises a fraction. A helper method.
+     *
+     * @param f a Tuple representing a numerator and denominator
+     */
+    private void normalise(Tuple f) {
+        int i;
+        i = this.GCD(Math.abs(f.n), Math.abs(f.d));
+        f.n = f.n / i;
+        f.d = f.d / i;
+    }
+
+    /**
+     * Manages how a negative is stored in a fraction. A helper method.
+     *
+     * @param f a Tuple representing a numerator and denominator
+     */
+    private void manage_negs(Tuple f) {
+        // If the denominator is negative
+        // and i) the numerator is negative then both become positive
+        // and ii) the numerator is not negative then both signs are flipped
+        if (f.d < 0) {
+            f.n *= -1;
+            f.d *= -1;
+        }
+    }
+
+    /**
+     * Finds the greatest common divisor using Euclid's algorithm
+     *
+     * @param a an integer
+     * @param b an integer
+     * @return greatest common divisor of a an b
      */
     private int GCD(int a, int b) {
         while (a != 0 && b != 0) {
@@ -243,23 +282,5 @@ public final class FractionImpl implements Fraction {
         }
         return (a == 0) ? b : a;
     }
-
-    /**
-     * @inheritDoc
-     */
-    private int splitN(String s) {
-        s = s.trim();
-        String mystring[] = s.split("/");
-        return Integer.parseInt(mystring[0]);
-    }
-    /**
-     * @inheritDoc
-     */
-    private int splitD(String s) {
-        s = s.trim();
-        String mystring[] = s.split("/");
-        return Integer.parseInt(mystring[1]);
-    }
-
 
 }
